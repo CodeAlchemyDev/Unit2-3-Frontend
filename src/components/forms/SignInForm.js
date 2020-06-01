6c52d9b8fffaaa1045bc025696e11efc7b8333f4
Amber Chunn <a.77@me.com>
2020-06-01 16:27:41 -0700

    Fixed Token Issue

--- src/components/forms/SignInForm.js
+++ src/components/forms/SignInForm.js
@@ -4,7 +4,6 @@ import { apiAuth } from "../../utils/apiAuth";
 import Validate from "./Validation";
 
 export default function SignInForm(props) {
-
   // Set up yup validation schema
   const formSchema = yup.object().shape({
     userName: yup
@@ -31,17 +30,17 @@ export default function SignInForm(props) {
   const inputChange = (e) => {
     e.persist();
     console.log(e.target.value);
-    Validate(formSchema, e, errorState, setErrorState)
+    Validate(formSchema, e, errorState, setErrorState);
     setFormState({ ...formState, [e.target.name]: e.target.value });
   };
 
   const formSubmit = (e) => {
     e.preventDefault();
     apiAuth()
-      .post("/login", { formState })
+      .post("/login", formState)
       .then((response) => {
         console.log(response);
-        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("token", response.data.Jwt);
         // props.history.push("/");
       })
       .catch((err) => {
@@ -51,15 +50,11 @@ export default function SignInForm(props) {
 
   return (
     <>
-
       <form onSubmit={formSubmit} className="signInForm">
-
         <h2 className="signInTitle">Sign In</h2>
 
         <label htmlFor="userName">
-
           Username:
-
           <input
             className="signInInput"
             type="text"
@@ -68,7 +63,6 @@ export default function SignInForm(props) {
             value={formState.userName}
             onChange={inputChange}
           />
-
         </label>
 
         {errorState.userName.length > 0 ? (
@@ -76,9 +70,7 @@ export default function SignInForm(props) {
         ) : null}
 
         <label htmlFor="password">
-
           Password:
-
           <input
             className="signInInput"
             type="password"
@@ -87,7 +79,6 @@ export default function SignInForm(props) {
             value={formState.password}
             onChange={inputChange}
           />
-
         </label>
 
         {errorState.password.length > 0 ? (
@@ -96,22 +87,13 @@ export default function SignInForm(props) {
 
         <button type="submit">Login</button>
 
-        <p className="newUserLink">New User?
-
-          <span
-            className='subLink'
-            onClick={props.registerClick}>
-
-          Click here to get started!
-
-        </span>
-
+        <p className="newUserLink">
+          New User?
+          <span className="subLink" onClick={props.registerClick}>
+            Click here to get started!
+          </span>
         </p>
-
       </form>
-
     </>
-
   );
-
 }
